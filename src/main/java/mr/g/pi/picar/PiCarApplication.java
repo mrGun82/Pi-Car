44de09b94a9155af88256c92b14c807acf0a58db
jiag gang <gun.mail@163.com>
2026-02-10 20:55:53 +0800

    start

---
+++ src/main/java/mr/g/pi/picar/PiCarApplication.java
@@ -0,0 +1,13 @@
+package mr.g.pi.picar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PiCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mr/g/pi/picar/PiCarApplicationTests.java
@@ -0,0 +1,13 @@
+package mr.g.pi.picar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
